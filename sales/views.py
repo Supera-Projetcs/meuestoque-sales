23dea3dc6841a8459cce917673714a66483f67a5
taysa-fernandes <taysamirelly34@gmail.com>
2024-04-05 13:17:56 -0300

    metodo de venda consumindo inventario

--- sales/views.py
+++ sales/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Sale
 from .serializers import SaleSerializer
+import requests
 
 @api_view(['GET', 'POST'])
 def sale_list(request):
@@ -19,31 +20,55 @@ def sale_list(request):
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = SaleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        sale_serializer = SaleSerializer(data=request.data)
+        if sale_serializer.is_valid():
+            product_id = request.data.get('inventory_id')
+            quantity = request.data.get('quantity')
+
+            product_url = f"http://inventorys/id/{product_id}"
+            try:
+                response = requests.get(product_url)
+                response.raise_for_status()  
+
+                product_data = response.json()
+
+                sale_data = {
+                    'product_name': product_data.get('product_name'),
+                    'quantity': quantity,
+                    'price': product_data.get('price'),
+                }
+
+                sale_serializer = SaleSerializer(data=sale_data)
+                if sale_serializer.is_valid():
+                    sale_serializer.save()
+                    return Response(sale_serializer.data, status=status.HTTP_201_CREATED)
+                else:
+                    return Response(sale_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except requests.exceptions.RequestException as e:
+                return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response(sale_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET'])
 def sales_report(request):
-    total_sales = Decimal(0)
+    total_sales_count = Sale.objects.count()  
+
+    product_info_url = "http://url-do-outro-servico/product/info"
+    try:
+        response = requests.get(product_info_url)
+        response.raise_for_status()  
+        product_info = response.json()
+        product_price = Decimal(product_info.get('price'))
+        best_selling_product_name = product_info.get('best_selling_product_name')
+    except requests.exceptions.RequestException as e:
+        return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    total_sales_price = Decimal(0)
     for sale in Sale.objects.all():
-        total_sales += sale.quantity * sale.price
-    
-    best_selling_product = Sale.objects.values('product_name').annotate(total_quantity=Sum('quantity')).order_by('-total_quantity').first()
-    all_products = Sale.objects.values('product_name').annotate(total_quantity=Sum('quantity'), total_price=Sum(F('quantity') * F('price')))
-    
-    if best_selling_product:
-        best_selling_product_name = best_selling_product['product_name']
-        best_selling_product_quantity = best_selling_product['total_quantity']
-    else:
-        best_selling_product_name = None
-        best_selling_product_quantity = 0
+        total_sales_price += sale.quantity * product_price
 
     return Response({
-        'total_sales': round(total_sales, 2),
+        'total_sales_count': total_sales_count,
+        'total_sales_price': round(total_sales_price, 2),
         'best_selling_product_name': best_selling_product_name,
-        'best_selling_product_quantity': best_selling_product_quantity,
-        'all_products': all_products
     })
